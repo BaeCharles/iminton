0ad4aa917e62f86ec2657c714d55ed77192986ba
unknown <young24y@hanmail.net>
2019-09-24 01:39:45 +0900

    출석리스트 수정

--- src/container/Attendance.js
+++ src/container/Attendance.js
@@ -7,7 +7,8 @@ import Layout from '../component/Layout';
 
 class Attendance extends Component {
     state = {
-        members: []
+        members: [],
+        nickname : ''
     }
 
     componentDidMount() {
@@ -23,42 +24,68 @@ class Attendance extends Component {
         return body;
     }
 
+    handleSubmit = (e) => {
+        e.preventDefault();
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            nickname: e.target.value,
+            //members: this.state.members.filter(data => (data.nickname === e.target.value))
+        });
+    }
+
+    _getList = () => {
+        this.setState({
+            members: (this.state.nickname === '') ? this.state.members : this.state.members.filter(data => (data.nickname === this.state.nickname))
+        });
+    }
+
     render() {
         return (
             <div>
                 <Layout>
                     <Form className='attendance_form' onSubmit={this.handleSubmit}>
                         <Form.Group controlId="nick">
-                            <FormControl type='date' value={new Date().toISOString().slice(0, 10)} />
-                            <InputGroup>
-                                <InputGroup.Prepend>
-                                    <InputGroup.Text><i class="fa fa-search"></i></InputGroup.Text>
-                                </InputGroup.Prepend>
-                                <FormControl placeholder="닉네임" />
-                                <InputGroup.Append>
-                                    <Button variant="outline-secondary">검색</Button>
-                                </InputGroup.Append>
-                            </InputGroup>
+                            <Form.Row>
+                                <Col xs={5.5}>
+                                    <Form.Control type='date' value={new Date().toISOString().slice(0, 10)} />
+                                </Col>
+                                <Col>
+                                    <InputGroup>
+                                        {/* <InputGroup.Prepend>
+                                            <InputGroup.Text><i class="fa fa-search"></i></InputGroup.Text>
+                                        </InputGroup.Prepend> */}
+                                        <Form.Control type="text" placeholder="닉네임" autocomplete="off" 
+                                            // ref={(input) => {this.textInput = input}}
+                                            value={this.state.nickname} 
+                                            onChange={this.handleChange} />
+                                        <InputGroup.Append>
+                                            <Button variant="outline-secondary" type="submit">검색</Button>
+                                        </InputGroup.Append>
+                                    </InputGroup>
+                                </Col>
+                            </Form.Row>
                         </Form.Group>
                     </Form>
-                    <Table striped bordered hover>
+                    <Table bordered hover size='sm'>
                         <thead>
                             <tr className='text-center'>
-                                <th width='50px'>no</th>
-                                <th width='30%'>닉네임</th>
-                                <th width='100px'>회원구분</th>
-                                <th width='60px'>출석</th>
-                                <th width='60px'>회비</th>
-                                <th width='120px'>콕판매</th>
+                                {/* <th width='50px'>no</th> */}
+                                <th>닉네임</th>
+                                {/* <th width='100px'>회원구분</th> */}
+                                <th width='50px'>출석</th>
+                                <th width='50px'>회비</th>
+                                <th width='80px'>콕판매</th>
                                 <th>비고</th>
                             </tr>
                         </thead>
                         <tbody>
                             {this.state.members.map((member, i) => (
                                 <tr className='text-center' key={i}>
-                                    <td>{i + 1}</td>
+                                    {/* <td>{i + 1}</td> */}
                                     <td className='text-left'>{member.nickname}</td>
-                                    <td>{member.grade}</td>
+                                    {/* <td>{member.grade}</td> */}
                                     <td><Button variant={(member.adate) ? 'dark' : 'outline-secondary'} size="sm"><i class='fa fa-user'></i></Button></td>
                                     <td><Button variant={(member.amount > 0) ? 'dark' : 'outline-secondary'} size="sm"><i class='fa fa-krw'></i></Button></td>
                                     <td>
